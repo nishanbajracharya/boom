142211131ab50b052da4230b45b6e28c8ab4fc0c
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2019-10-31 20:29:12 +0545

    Updates

---
+++ precache-manifest.5feee80f056c8639cdcde34f9391b144.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7ebdbf8bee97c7b5e21fb5cd6f43ca",
+    "url": "/boom/index.html"
+  },
+  {
+    "revision": "5293bb18a0d8052fea9a",
+    "url": "/boom/static/css/main.768f9500.chunk.css"
+  },
+  {
+    "revision": "61bde075159fd9cb6033",
+    "url": "/boom/static/js/2.0ae2e6fb.chunk.js"
+  },
+  {
+    "revision": "5293bb18a0d8052fea9a",
+    "url": "/boom/static/js/main.97295e19.chunk.js"
+  },
+  {
+    "revision": "9b8a3429703d80411554",
+    "url": "/boom/static/js/runtime-main.065612d4.js"
+  }
+]);
